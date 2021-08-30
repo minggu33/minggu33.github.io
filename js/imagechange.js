08da9c71b286a3feef2c623d2442f26f1817eb69
minggu33 <81955209+minggu33@users.noreply.github.com>
2021-08-30 18:46:53 +0900

    Update imagechange.js

--- js/imagechange.js
+++ js/imagechange.js
@@ -1,1 +1,13 @@
-var image
+var myImage = document.getElementById("mainImage");
+	var imageArray = [ "images/dbaproject.png",
+			"images/dbaproject1.png","images/dbaproject2.png" ];
+	var imageIndex = 0;
+
+	function changeImage() {
+		myImage.setAttribute("src", imageArray[imageIndex]);
+		imageIndex++;
+		if (imageIndex >= imageArray.length) {
+			imageIndex = 0;
+		}
+	}
+	setInterval(changeImage, 3000);
