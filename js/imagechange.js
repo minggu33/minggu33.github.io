8cd3ffabf7d760fddaa5fa241621d3403e37cf04
minggu33 <81955209+minggu33@users.noreply.github.com>
2021-08-31 14:15:58 +0900

    Update imagechange.js

--- js/imagechange.js
+++ js/imagechange.js
@@ -11,3 +11,19 @@ var myImage = document.getElementById("mainImage");
 		}
 	}
 	setInterval(changeImage, 3000);
+
+var myImage1 = document.getElementById("mainImage1");
+	var imageArray1 = [ "images/teamproject1.png",
+			"images/teamproject2.png","images/teamproject3.png" ];
+	var imageIndex1 = 0;
+
+	function changeImage1() {
+		myImage1.setAttribute("src", imageArray1[imageIndex1]);
+		imageIndex1++;
+		if (imageIndex1 >= imageArray1.length) {
+			imageIndex1 = 0;
+		}
+	}
+	setInterval(changeImage1, 3000);
+
+
